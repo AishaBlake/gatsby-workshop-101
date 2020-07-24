868a0d8bb89fb1c1b09324c3240997fa5dc1b4d4
Aisha Blake <aisha.g.blake@gmail.com>
2020-07-24 15:20:16 -0400

    Add blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,28 @@
 module.exports = {
+    siteMetadata: {
+        title: `Aisha Blake`,
+        author: `Aisha Blake`,
+        description: `Aisha Blake's personal site for Gatsby 101 at CodeLand`,
+        siteUrl: `https://workshop-site-101.netlify.app`,
+        social: [
+            {
+                name: `Twitter`,
+                url: `https://twitter.com/AishaBlake`,
+            },
+            {
+                name: `GitHub`,
+                url: `https://github.com/AishaBlake`,
+            },
+        ],
+    },
     plugins: [
-        `gatsby-plugin-mdx`
+        `gatsby-plugin-mdx`,
+        {
+            resolve: `gatsby-theme-blog`,
+            options: {
+                basePath: `/blog`,
+                mdxOtherwiseConfigured: true
+            }
+        }
     ]
 }
